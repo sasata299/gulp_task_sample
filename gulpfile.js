0ebb9f0334dacfeefa05be3a39a6c6c9fafa1336
sasata299 <karaage299@gmail.com>
2016-11-06 09:59:24 +0900

    コンパイル／CSSの結合／CSSの圧縮／リネームまで一連の処理を行う

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 gulp.task('stylus', function(){
   return gulp.src('styl/**/*.styl')
     .pipe(stylus())
+    .pipe(concat('all.css'))
+    .pipe(minify())
+    .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('css/'));
 });
 
