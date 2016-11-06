42a40e82659eac2fed2c74af53873a1f09126d87
sasata299 <karaage299@gmail.com>
2016-11-06 09:52:55 +0900

    gulp-stylusを導入して簡単なタスクを追加する

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+gulp.task('stylus', function(){
+  return gulp.src('styl/**/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('css/'));
+});
 
 gulp.task('hello', function() {
   console.log('Hello gulp!');
