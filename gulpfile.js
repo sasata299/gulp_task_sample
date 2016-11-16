a06bdbab730a0c60290049ba8c5c8c840dc85218
sasata299 <karaage299@gmail.com>
2016-11-16 21:47:44 +0900

    外部コマンドを実行する

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,19 @@ var browserSync = require('browser-sync');
 var using = require('gulp-using');
 var cached = require('gulp-cached');
 var remember = require('gulp-remember');
+var exec = require('child_process').exec;
+
+gulp.task('test', function() {
+  console.log('test');
+});
+
+gulp.task('exec', function(cb) {
+  exec('gulp test', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
 
 function plumberWithNotify() {
   return plumber({ errorHandler: notify.onError("<%= error.message %>") });
